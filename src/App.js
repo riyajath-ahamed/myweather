b6caa908f0cca688c3a76061d1f9ce326a8c540d
riyajath-ahamed <riyajatha@gmail.com>
2023-03-16 14:12:22 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Card } from './components'
+import  sunnycloud  from "./assets/sunnycloud.png";
+import axios from "axios";
 
-function App() {
+const App = () => {
+
+
+
+const options = {
+  method: 'GET',
+  url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities/Q60',
+  headers: {
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='absolute inset-0 bg-white flex items-center justify-center p-4 '>
+      
+     
+      <div className=" w-full md:w-375 p-4  bg-lightOverlay shadow-2xl rounded-xl backdrop-blur-md flex-col items-center justify-center"> 
+      <div>
+        <img src={sunnycloud} />
+        <p className='font-bold text-textColor text-7xl float-right'>32°c</p>
+      </div>
+      
+      </div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const card = () => {
+
+const bg1 ="https://i.pinimg.com/originals/ac/6d/c4/ac6dc4fe1925ca65a92fb0bc88cfe6ba.gif"
+
+  return (
+    <video
+        src={"https://i.pinimg.com/originals/ac/6d/c4/ac6dc4fe1925ca65a92fb0bc88cfe6ba.gif"}
+        type="video/mp4"
+        autoPlay
+        muted
+        loop
+        className="w-full h-full object-cover"
+      ></video>
+  )
+}
+
+export default card
